0fb2c337297d360d124026a94ed50368c40d9557
AbedKarmi <abedkarmi@gmail.com>
2026-05-01 10:48:58 +0300

    Ver 6.6.0

--- Extensions/dnSpy.AIChat/UI/ChatMessage.cs
+++ Extensions/dnSpy.AIChat/UI/ChatMessage.cs
@@ -1,3 +1,5 @@
+using System.Windows;
+using System.Windows.Input;
 using dnSpy.Contracts.MVVM;
 
 namespace dnSpy.AIChat.UI {
@@ -10,9 +12,12 @@ namespace dnSpy.AIChat.UI {
 			private set { if (content != value) { content = value; OnPropertyChanged(nameof(Content)); } }
 		}
 
+		public ICommand CopyCommand { get; }
+
 		public ChatMessage(string role, string content) {
 			Role = role;
 			this.content = content;
+			CopyCommand = new RelayCommand(_ => Clipboard.SetText(Content));
 		}
 
 		public void Append(string text) => Content += text;
